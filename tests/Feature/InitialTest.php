0f510b95f46738c10fd2987fba45511b4e4bb3e5
Jithran <info@jithransikken.nl>
2023-07-23 22:01:28 +0200

    -add: php unit scaffolding

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jithran\GitRibbon\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function testInitial()
+    {
+        $this->assertTrue(true);
+    }
+}
